e967d57fe89a529bcb9d1328f85bb9e31b426380
Maximilian6547s <maximilian.s517@gmail.com>
2021-07-01 21:10:37 +0300

    Setup Jetpack Compose

--- app/src/main/java/com/maximcuker/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/maximcuker/mvvmrecipeapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.maximcuker.mvvmrecipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.*
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "Hey look some text")
+                Spacer(modifier = Modifier.padding(top= 10.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "A BUTTON")
+                }
+            }
+        }
     }
 }
