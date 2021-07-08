4dd8d07b65ce751b38fd2466258137eb4b81dd60
Maximilian6547s <maximilian.s517@gmail.com>
2021-07-08 10:46:08 +0300

    Circular progress Bar using Composable Constraint Layout

--- app/src/main/java/com/maximcuker/mvvmrecipeapp/presentation/components/CircularIndeterminateProgressBar.kt
+++ app/src/main/java/com/maximcuker/mvvmrecipeapp/presentation/components/CircularIndeterminateProgressBar.kt
@@ -1,28 +1,33 @@
 package com.maximcuker.mvvmrecipeapp.presentation.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.WithConstraints
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import java.nio.file.WatchEvent
 
 @Composable
-fun CircularIndeterminateProgressBar(
-    isDisplayed: Boolean
-) {
+fun CircularIndeterminateProgressBar(isDisplayed: Boolean) {
     if (isDisplayed) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp),
-            horizontalArrangement = Arrangement.Center
+        ConstraintLayout(
+            modifier = Modifier.fillMaxSize()
         ) {
+            val progress = createRef()
+            val topGuideline = createGuidelineFromTop(0.3f)
             CircularProgressIndicator(
+                modifier = Modifier.constrainAs(progress) {
+                    top.linkTo(topGuideline)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
                 color = MaterialTheme.colors.primary
             )
         }
