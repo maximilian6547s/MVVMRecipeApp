6352d77c57e15f1e44ade6a86358a384e80b7f5e
Maximilian6547s <maximilian.s517@gmail.com>
2021-07-19 18:12:23 +0300

    Monitoring a Network Connection with ConnectivityManager example

--- app/src/main/java/com/maximcuker/mvvmrecipeapp/presentation/MainActivity.kt
+++ app/src/main/java/com/maximcuker/mvvmrecipeapp/presentation/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.maximcuker.mvvmrecipeapp.presentation
 
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.ui.platform.AmbientContext
 import androidx.compose.ui.platform.setContent
@@ -18,6 +23,36 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint //entry point also must be in an activity(host)
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "c-Manager"
+    lateinit var cm: ConnectivityManager
+
+    val networkRequest = NetworkRequest.Builder()
+        .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        .build()
+
+    val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            Log.d(TAG, "onAvailable: ${network}")
+        }
+
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            Log.d(TAG, "onLost: ${network}")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        cm = this.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        cm.registerNetworkCallback(networkRequest, networkCallback)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cm.unregisterNetworkCallback(networkCallback)
+    }
 
     @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
