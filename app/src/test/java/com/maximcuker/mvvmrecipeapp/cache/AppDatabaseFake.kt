c7c36276d0a9ac928d2bbc121a0d84ae48692dfd
Maximilian6547s <maximilian.s517@gmail.com>
2021-07-20 11:56:19 +0300

    Fake Caching Data

---
+++ app/src/test/java/com/maximcuker/mvvmrecipeapp/cache/AppDatabaseFake.kt
@@ -0,0 +1,11 @@
+package com.maximcuker.mvvmrecipeapp.cache
+
+import com.maximcuker.mvvmrecipeapp.cashe.model.RecipeEntity
+
+class AppDatabaseFake {
+
+    // fake for recipe table in local db
+    val recipes = mutableListOf<RecipeEntity>()
+
+
+}
--- app/src/test/java/com/maximcuker/mvvmrecipeapp/interactors/recipe_list/SearchRecipesTest.kt
+++ app/src/test/java/com/maximcuker/mvvmrecipeapp/interactors/recipe_list/SearchRecipesTest.kt
@@ -1,8 +1,12 @@
 package com.maximcuker.mvvmrecipeapp.interactors.recipe_list
 
 import com.google.gson.GsonBuilder
+import com.maximcuker.mvvmrecipeapp.cache.AppDatabaseFake
+import com.maximcuker.mvvmrecipeapp.cache.RecipeDaoFake
+import com.maximcuker.mvvmrecipeapp.cashe.model.RecipeEntityMapper
 import com.maximcuker.mvvmrecipeapp.network.RecipeService
 import com.maximcuker.mvvmrecipeapp.network.data.MockWebServerResponses.recipeListResponse
+import com.maximcuker.mvvmrecipeapp.network.model.RecipeDtoMapper
 import okhttp3.HttpUrl
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
@@ -18,12 +22,17 @@ class SearchRecipesTest {
 
     private lateinit var mockWebServer: MockWebServer
     private lateinit var baseUrl: HttpUrl
+    private val appDataBase = AppDatabaseFake()
 
     // system in test
-//    private val searchRecipes:SearchRecipes
+    private lateinit var searchRecipes:SearchRecipes
 
     // dependencies
     private lateinit var recipeService: RecipeService
+    private lateinit var recipeDao: RecipeDaoFake
+    private val dtoMapper = RecipeDtoMapper()
+    private val entityMapper = RecipeEntityMapper()
+
 
     @BeforeEach
     fun setup() {
@@ -35,6 +44,16 @@ class SearchRecipesTest {
             .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
             .build()
             .create(RecipeService::class.java)
+
+        recipeDao = RecipeDaoFake(appDataBase)
+
+        // instantiate the system in test
+        searchRecipes = SearchRecipes(
+            recipeDao = recipeDao,
+            recipeService = recipeService,
+            entityMapper = entityMapper,
+            dtoMapper = dtoMapper
+        )
     }
 
     @Test
