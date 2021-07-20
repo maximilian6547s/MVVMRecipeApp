37dfb6d0266d223d4460c839ec25c73cd7f474f6
Maximilian6547s <maximilian.s517@gmail.com>
2021-07-20 12:54:17 +0300

    Testing RestoreRecipes Use-case

--- app/src/test/java/com/maximcuker/mvvmrecipeapp/interactors/recipe_list/RestoreRecipesTest.kt
+++ app/src/test/java/com/maximcuker/mvvmrecipeapp/interactors/recipe_list/RestoreRecipesTest.kt
@@ -6,7 +6,7 @@ import com.maximcuker.mvvmrecipeapp.cache.RecipeDaoFake
 import com.maximcuker.mvvmrecipeapp.cashe.model.RecipeEntityMapper
 import com.maximcuker.mvvmrecipeapp.domain.model.Recipe
 import com.maximcuker.mvvmrecipeapp.network.RecipeService
-import com.maximcuker.mvvmrecipeapp.network.data.MockWebServerResponses.recipeListResponse
+import com.maximcuker.mvvmrecipeapp.network.data.MockWebServerResponses
 import com.maximcuker.mvvmrecipeapp.network.model.RecipeDtoMapper
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
@@ -20,23 +20,23 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.net.HttpURLConnection
 
-class SearchRecipesTest {
+class RestoreRecipesTest {
 
+    private val appDatabase = AppDatabaseFake()
     private lateinit var mockWebServer: MockWebServer
     private lateinit var baseUrl: HttpUrl
-    private val appDataBase = AppDatabaseFake()
     private val DUMMY_TOKEN = "gg335v5453453" // can be anything
     private val DUMMY_QUERY = "This doesn't matter" // can be anything
 
     // system in test
-    private lateinit var searchRecipes: SearchRecipes
+    private lateinit var restoreRecipes: RestoreRecipes
 
-    // dependencies
+    // Dependencies
+    private lateinit var searchRecipes: SearchRecipes
     private lateinit var recipeService: RecipeService
     private lateinit var recipeDao: RecipeDaoFake
-    private val dtoMapper = RecipeDtoMapper()
     private val entityMapper = RecipeEntityMapper()
-
+    private val dtoMapper = RecipeDtoMapper()
 
     @BeforeEach
     fun setup() {
@@ -48,38 +48,47 @@ class SearchRecipesTest {
             .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
             .build()
             .create(RecipeService::class.java)
+        recipeDao = RecipeDaoFake(appDatabaseFake = appDatabase)
 
-        recipeDao = RecipeDaoFake(appDataBase)
-
-        // instantiate the system in test
         searchRecipes = SearchRecipes(
             recipeDao = recipeDao,
             recipeService = recipeService,
             entityMapper = entityMapper,
             dtoMapper = dtoMapper
         )
+
+        // instantiate system in test
+        restoreRecipes = RestoreRecipes(
+            recipeDao = recipeDao,
+            entityMapper = entityMapper
+        )
     }
 
     /**
-     * 1. Are the recipes retrieved from network
-     * 2. Are the recipes inserted into the cache
-     * 3. Are the recipes then emitted as a flow from the cache to UI
+     * 1. Get some recipes from the network and insert into cache
+     * 2. Restore and show recipes are retrieved from cache
      */
     @Test
-    fun getRecipesFromNetwork_emitRecipesFromCache(): Unit = runBlocking {
+    fun getRecipesFromNetwork_restoreFromCache(): Unit = runBlocking {
+
         // condition the response
         mockWebServer.enqueue(
             MockResponse()
                 .setResponseCode(HttpURLConnection.HTTP_OK)
-                .setBody(recipeListResponse)
+                .setBody(MockWebServerResponses.recipeListResponse)
         )
+
         // confirm the cache is empty to start
         assert(recipeDao.getAllRecipes(1, 30).isEmpty())
 
-        val flowItems = searchRecipes.execute(DUMMY_TOKEN, 1, true, DUMMY_QUERY).toList()
+        // get recipes from network and insert into cache
+        val searchResult = searchRecipes.execute(DUMMY_TOKEN, 1, true, DUMMY_QUERY).toList()
 
         // confirm the cache is no longer empty
         assert(recipeDao.getAllRecipes(1, 30).isNotEmpty())
+
+        // run use case
+        val flowItems = restoreRecipes.execute(1, DUMMY_QUERY).toList()
 
         // first emission should be `loading`
         assert(flowItems[0].loading)
@@ -89,39 +98,14 @@ class SearchRecipesTest {
         assert(recipes?.size ?: 0 > 0)
 
         // confirm they are actually Recipe objects
-        assert(recipes?.get(index = 0) is Recipe)
+        assert(value = recipes?.get(index = 0) is Recipe)
 
         assert(!flowItems[1].loading) // loading should be false now
     }
 
-    /**
-     * Simulate a bad request
-     */
-    @Test
-    fun getRecipesFromNetwork_emitHttpError(): Unit = runBlocking {
-
-        // condition the response
-        mockWebServer.enqueue(
-            MockResponse()
-                .setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST)
-                .setBody("{}")
-        )
-
-        val flowItems = searchRecipes.execute(DUMMY_TOKEN, 1, true, DUMMY_QUERY).toList()
-
-        // first emission should be `loading`
-        assert(flowItems[0].loading)
-
-        // Second emission should be the exception
-        val error = flowItems[1].error
-        assert(error != null)
-
-        assert(!flowItems[1].loading) // loading should be false now
-    }
 
     @AfterEach
     fun tearDown() {
         mockWebServer.shutdown()
     }
-
 }
